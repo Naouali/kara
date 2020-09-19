e8d39b43f35c1e72edd07c84092a2be93defe9f6
Naouali <elnaoualinebil@gmail.com>
2020-09-19 14:48:38 +0100

    kara ready

--- main.py
+++ main.py
@@ -6,12 +6,55 @@ speech_recognition module
 pyaudi
 gtts
 """
-
+from time import ctime
 import speech_recognition as sr
-r = sr.Recognizer()
+import webbrowser
+import time
+import os
+import playsound
+import random
+from gtts import gTTS
 
-with sr.Microphone() as source:
-    print("say something")
-    audio = r.listen(source)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
+
+r = sr.Recognizer()
+def record(ask=False):
+    with sr.Microphone() as source:
+        if ask:
+            kara_speak(ask)
+        audio = r.listen(source)
+        voice_data =""
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            kara_speak(" Sorry I didn't understand")
+        except sr.RequestError:
+            kara_speak("Sorry, i'm speeling")
+        return voice_data
+
+def kara_speak(audio):
+    tts = gTTS(text=audio, lang='en')
+    r = random.randint(1, 10000000)
+    audio_file = 'audio-' + str(r) + 'mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio)
+    os.remove(audio_file)
+
+
+def respond(voice_data):
+    if 'hello' in voice_data:
+        kara_speak("Hello master")
+    if 'what time is it' in voice_data:
+        kara_speak(ctime())
+    if 'search' in voice_data:
+        search = record("What do want to search for?")
+        url = "https://google.com/search?q=" + search
+        webbrowser.get().open(url)
+    if 'exit' in voice_data:
+        exit()
+time.sleep(1)
+kara_speak("How can I help you")
+
+while True:
+    my_audio =  record()
+    print(respond(my_audio))
